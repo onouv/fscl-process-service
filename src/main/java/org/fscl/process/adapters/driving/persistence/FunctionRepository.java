4b9b375b0ba2504dcafcf06000e1817b44fa60da
olaf <olaf.nouvortne@onosoft.online>
2025-11-21 13:27:18 +0100

    create function working short of a) message publishing, b) name and description fields persisting

--- src/main/java/org/fscl/process/adapters/driving/persistence/FunctionRepository.java
+++ src/main/java/org/fscl/process/adapters/driving/persistence/FunctionRepository.java
@@ -1,29 +1,29 @@
 package org.fscl.process.adapters.driving.persistence;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.fscl.core.commons.ResourceIdDataDto;
 
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class FunctionRepository implements PanacheRepository<FunctionDataDto> {
+public class FunctionRepository implements PanacheRepositoryBase<FunctionDataDto, ResourceIdDataDto> {
 
-	public Optional<FunctionDataDto> findById(ResourceIdDataDto id) throws PersistenceException {
-		final PanacheQuery<FunctionDataDto> q = find(
-				"SELECT f FROM functions f WHERE f.entityid_project='?1' AND f.entityid_code='?2'", id.getProject(),
-				id.getCode());
+	/*
+	 * public Optional<FunctionDataDto> findById(ResourceIdDataDto id) throws
+	 * PersistenceException { final PanacheQuery<FunctionDataDto> q =
+	 * find("FROM FunctionDataDto WHERE project=?1 AND code=?2", id.getProject(),
+	 * id.getCode());
+	 * 
+	 * return q.firstResultOptional(); }
+	 */
 
-		return q.firstResultOptional();
+	public List<FunctionDataDto> findByIdProject(String projectId) {
+		return list("FROM FunctionDataDto WHERE project=?", projectId);
 	}
-
-	public List<FunctionDataDto> findAllForProject(String projectId) {
-		final PanacheQuery<FunctionDataDto> q = find("entityid_project = ?1", projectId);
-
-		return q.list();
-	}
+	/*
+	 * Optional<FunctionDataDto> findById(ResourceIdDataDto outwards);
+	 */
 
 }
